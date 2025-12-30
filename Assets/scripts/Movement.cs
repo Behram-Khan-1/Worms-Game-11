c56e9aba97b7e6c0ba3c84d1285b67d89501cfe1
Behram Khan <khanbehram66@gmail.com>
2025-12-30 19:53:38 +0500

    Moving along ground for now
    Will comeback to fix movement.

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,13 +8,14 @@ public class Movement : MonoBehaviour
     public float movementSpeed = 5f;
     public float jumpForce = 5f;
     private float inputHorizontal;
-    private bool isJumped;
+    private Vector2 mousePosition;
 
-    private CircleCollider2D circleCollider2D;
-    private float colliderRadius;
+
+
     private Rigidbody2D rb;
     RaycastHit2D ray;
     public float groundOffset = 0.11f;
+    public float rayDistance = 0.11f;
 
 
 
@@ -23,7 +25,7 @@ public class Movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+
     }
 
     // Update is called once per frame
@@ -32,17 +34,11 @@ public class Movement : MonoBehaviour
         inputHorizontal = Input.GetAxisRaw("Horizontal");
         GroundCheck();
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
-            isJumped = true;
-        }
-
-        // if (isGrounded && isJumped == false)
+        // if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         // {
-        //     GroundRayCast();
+        //     mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition).normalized;
+        //     rb.AddForce(mousePosition * jumpForce, ForceMode2D.Impulse);
         // }
-
 
 
     }
@@ -51,20 +47,26 @@ public class Movement : MonoBehaviour
     {
         if (inputHorizontal != 0)
         {
-            rb.linearVelocityX = inputHorizontal * movementSpeed * Time.fixedDeltaTime;
-            SnapToGround();
+            var tangent = Vector3.Cross(ray.normal, transform.forward); 
+            Vector2 tangentV2 = tangent;
+            rb.linearVelocity =  tangentV2 * inputHorizontal * movementSpeed * Time.fixedDeltaTime;
         }
+        if (isGrounded)
+            SnapToGround();
     }
 
     void GroundCheck()
     {
-        ray = Physics2D.Raycast(transform.position, -transform.up, groundOffset, LayerMask.GetMask("Ground"));
-        Debug.DrawRay(transform.position, -transform.up * groundOffset, Color.red);
+        RaycastHit2D rayDown = Physics2D.Raycast(transform.position, -transform.up, rayDistance, LayerMask.GetMask("Ground"));
+
+        ray = rayDown;
+
+        Debug.DrawRay(transform.position, -transform.up * rayDistance, Color.green);
+
         if (ray.collider != null)
         {
             isGrounded = true;
             rb.gravityScale = 0;
-            isJumped = false;
 
         }
         else
@@ -76,44 +78,19 @@ public class Movement : MonoBehaviour
 
     void SnapToGround()
     {
-        if (isGrounded)
+        if (ray.collider != null)
         {
-            if (ray.collider != null)
-            {
-                transform.rotation = Quaternion.LookRotation(Vector3.forward, ray.normal);
-                transform.position = ray.point + ray.normal * groundOffset;
-            }
-        }
+            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, ray.normal);
+            transform.rotation =  Quaternion.Lerp(transform.rotation, rotation, 0.5f);
 
+
+
+            // transform.position = ray.point + ray.normal * groundOffset;
+
+            // rb.MoveRotation(Quaternion.LookRotation(Vector3.forward, ray.normal));
+            // rb.MovePosition(ray.point + ray.normal * groundOffset);
+        }
     }
 
 
-    // private void GroundRayCast()
-    // {
-    //     Debug.DrawRay(transform.position, -transform.up * 0.3f, Color.red);
-
-
-    //     Debug.Log(ray.point);
-    //     Debug.Log(ray.normal);
-
-    //     // ?????????????????????????????????????????
-    //     // Vector2 v = rb.linearVelocity;
-    //     // v -= Vector2.Dot(v, ray.normal) * ray.normal;
-    //     // rb.linearVelocity = v;
-
-
-    // }
-    //     else
-    //     {
-    //         rb.gravityScale = 1;
-    //     }
-    // }
-
-
-
-
-    // private bool GroundCheck()
-    // { 
-    //     if( 
-    // }
 }
